875721f2463fda9072d7fe43fcbc2b727d55d44c
Jeff Walton <jwalton89@gmail.com>
2017-11-28 01:56:43 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,3 +6,8 @@ var songTitle = 'Halo'
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, {artistName: songTitle})
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
+  return playlist
+}
