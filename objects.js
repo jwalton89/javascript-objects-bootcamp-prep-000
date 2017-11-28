648a7ef166c6799fa1bcd19a4d6b563fad699fed
Jeff Walton <jwalton89@gmail.com>
2017-11-28 01:55:05 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,2 +1,8 @@
 var playlist = {'dirty projectors':'up in hudson', 'rick astley':'never gonna give you up', prince:'purple rain'}
 var object = playlist
+var artistName = 'Beyonce'
+var songTitle = 'Halo'
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  
+}
